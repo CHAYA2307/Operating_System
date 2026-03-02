f9cde006b9a39fadb5a10d7c40be3cc4631199ea
CHAYA2307 <chaya.cs24@bmsce.ac.in>
2026-03-02 12:30:29 +0530

    Add files via upload

---
+++ week1p4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int count = 0;
+    for(int i = 0; i < n; i++) {
+        for(int j = i+1; j < n; j++) {
+            if(arr[i] == arr[j]) {
+                count++;
+                break;
+            }
+        }
+    }
+
+    printf("Total number of duplicate elements: %d\n", count);
+    return 0;
+}
